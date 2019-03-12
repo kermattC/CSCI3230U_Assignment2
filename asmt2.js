1fb9f139213fdf4360b3fc04dd1c461f7a20e114
domcabitac <dominic.cabitac@uoit.net>
2019-03-12 17:31:56 -0400

    Used button to load keys

--- asmt2.js
+++ asmt2.js
@@ -1,10 +1,15 @@
-$.getJSON('http://api.apixu.com/v1/forecast.json?key=fe34f785ddd3406dbbf202145191203&q=Paris', function(data) {
-    $.each(data, function(key, value) {
-        if (key == "location"){
-            console.log(value.name);
-        }
-        if (key == "current"){
-            console.log(value.wind_mph);
-        }
+$(function(){
+    $("#goButton").on('click', function(){
+        $.getJSON('http://api.apixu.com/v1/forecast.json?key=fe34f785ddd3406dbbf202145191203&q=Paris', function(data) {
+            $.each(data, function(key, value) {
+                if (key == "location"){
+                    console.log(value.name);
+                }
+                if (key == "current"){
+                    console.log(value.wind_mph);
+                }
+            });
+        });
     });
-});
+}) ;
+
