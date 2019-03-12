306753107c2e468c85c746513f0805ae0e84082b
kermattC <matthew.chan5@uoit.net>
2019-03-12 17:38:05 -0400

    Added forecast info

--- asmt2.js
+++ asmt2.js
@@ -1,10 +1,23 @@
-$.getJSON('http://api.apixu.com/v1/forecast.json?key=fe34f785ddd3406dbbf202145191203&q=Paris', function(data) {
-    $.each(data, function(key, value) {
-        if (key == "location"){
-            console.log(value.name);
-        }
-        if (key == "current"){
-            console.log(value.wind_mph);
-        }
+$(document).ready(function () {
+    $.getJSON('http://api.apixu.com/v1/current.json?q=43.944847,-78.891703&days=7&key=fe34f785ddd3406dbbf202145191203', function(data) {
+        $.each(data, function(key, value) {
+            if (key == "location"){
+                console.log(value.name);
+            }
+            if (key == "current"){
+                console.log(value.wind_mph);
+            }
+        });
+    });
+    $.getJSON('http://api.apixu.com/v1/forecast.json?q=43.944847,-78.891703&days=7&key=fe34f785ddd3406dbbf202145191203', function(data) {
+        $.each(data, function(key, value) {
+            if (key == "location"){
+                console.log(value.name);
+            }
+            if (key == "current"){
+                console.log(value.wind_mph);
+            }
+
+        });
     });
 });
