5dbbfa7aaaa5c30a7eee62505c7e918cc5d15f51
kermattC <matthew.chan5@uoit.net>
2019-03-12 17:20:53 -0400

    Added method of finding individual data

--- images/asmt2.js
+++ images/asmt2.js
@@ -1,10 +1,5 @@
 $.getJSON('http://api.apixu.com/v1/forecast.json?key=fe34f785ddd3406dbbf202145191203&q=Paris', function(data) {
     $.each(data, function(key, value) {
-        if (key == "location"){
-            console.log(value.name);
-        }
-        if (key == "current"){
-            console.log(value.wind_mph);
-        }
+        console.log(key + ' ' + stuff);
     });
 });
