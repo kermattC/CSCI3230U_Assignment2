fb1478fd467e80ba62513e2301f6c5e41836c00a
domcabitac <dominic.cabitac@uoit.net>
2019-03-12 17:16:03 -0400

    Update asmt2.js

--- asmt2.js
+++ asmt2.js
@@ -1,5 +1,5 @@
 $.getJSON('http://api.apixu.com/v1/forecast.json?key=fe34f785ddd3406dbbf202145191203&q=Paris', function(data) {
     $.each(data, function(key, value) {
-        console.log(key + ' ' + value);
+        console.log(key, ' ', value);
     });
 });
